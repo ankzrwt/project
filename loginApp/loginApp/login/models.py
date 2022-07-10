2ee5e554b2ba44c92707511e79eb38de4a969f52
ankzrwt <100846273+ankzrwt@users.noreply.github.com>
2022-07-10 08:59:51 +0530

    created user model

--- loginApp/loginApp/login/models.py
+++ loginApp/loginApp/login/models.py
@@ -1,3 +1,9 @@
+from asyncio.windows_events import NULL
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name=models.CharField(max_length=50)
+    password=models.CharField(max_length=50,null=False)
+    userName=models.CharField(max_length=50,null=False)
+    
